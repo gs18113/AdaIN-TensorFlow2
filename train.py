ed91e1fe3d8eef2a0dbabc0cb33a3bd34ccb3e68
gs18113 <seungmin960@gmail.com>
2019-10-28 21:00:53 +0900

    asdf

--- train.py
+++ train.py
@@ -81,6 +81,7 @@ if args.save_tflite:
 
 converter = tf.lite.TFLiteConverter.from_concrete_functions([model.call.get_concrete_function()])
 tflite_model = converter.convert()
+tf.saved_model.save(model, 'saved_models/test/asdf')
 print('converted!!')
 '''
 logging.info('All ready, starting train steps')
