d45f078a75e1d47d5ac749d18fe0721c8ed9cbe9
gs18113 <seungmin960@gmail.com>
2019-10-28 18:08:45 +0900

    Using tf checkpoints

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ from os.path import join, exists
 from model import Net, get_decoder
 from data import get_training_set, get_test_set
 import pickle
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [INFO] %(message)s')
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -20,7 +23,6 @@ def str2bool(v):
 parser = argparse.ArgumentParser()
 parser.add_argument('-style_dir', type=str, default='style_images')
 parser.add_argument('-save_dir', type=str, default='saved_models')
-parser.add_argument('-exp_name', type=str, required=True)
 parser.add_argument('-lr', default=1e-4, type=float)
 parser.add_argument('-lr_decay', default=5e-5, type=float)
 parser.add_argument('-exp_name', type=str, required=True)
@@ -28,7 +30,7 @@ parser.add_argument('-batch_size', type=int , default=8)
 parser.add_argument('-max_iter', type=int, default=160000)
 parser.add_argument('-style_weight', type=float, default=10.0)
 parser.add_argument('-content_weight', type=float, default=1.0)
-parser.add_argument('-save_model_weights', type=str2bool, default=False)
+parser.add_argument('-save_ckpt', type=str2bool, default=False)
 parser.add_argument('-save_tflite', type=str2bool, default=False)
 parser.add_argument('-save_every', type=int , default=10000)
 args = parser.parse_args()
@@ -63,28 +65,44 @@ def train_step(content_images, style_images, step):
         tf.summary.scalar("loss_c", loss_c, step=i)
         tf.summary.scalar("loss_s", loss_s, step=i)
 
+# Checkpoints
+ckpt = None
+manager = None
+if args.save_ckpt:
+    ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=optimizer, net=model)
+    manager = tf.train.CheckpointManager(ckpt, join(args.save_dir, args.exp_name, 'ckpts/'), max_to_keep=100)
+
+logging.info('All ready, starting train steps')
 with writer.as_default():
     for i in tqdm(range(args.max_iter)):
         content_images, style_images = next(train_iter)
         train_step(content_images, style_images, i)
         writer.flush()
+        if args.save_ckpt:
+            ckpt.step.assign_add(1)
         if (i+1) % args.save_every == 0 or (i+1) == args.max_iter:
-            save_path = join(join(args.save_dir, args.exp_name), str(i))
+            save_path = join(args.save_dir, args.exp_name, str(i))
+            logging.info('Saving model to %s' % save_path)
             if not exists(save_path):
                 os.makedirs(save_path)
             tf.saved_model.save(model, save_path)
-            if args.save_model_weights:
-                weight_file = join(join(args.save_dir, args.exp_name+'_weights'), str(i)+'_weights.h5')
-                model.save_weights(weight_file, save_format='tf')
+            if args.save_ckpt:
+                #weight_file = join(args.save_dir, args.exp_name+'_weights', str(i)+'_weights.h5')
+                # logging.info('Saving weights to %s' % weight_file)
 
-                # optimizer weights
-                optimizer_file = join(join(args.save_dir, args.exp_name+'_weights'), str(i)+'_optimizer.pkl')
-                optimizer_weights = getattr(optimizer, 'weights')
-                weight_values = tf.keras.batch_get_value(optimizer_weights)
-                with open(optimizer_file, 'wb') as f:
-                    pickle.dump(weight_values, f)
+                # model.save_weights(weight_file, save_format='tf')
+
+                # # optimizer weights
+                # optimizer_file = join(args.save_dir, args.exp_name+'_weights', str(i)+'_optimizer.pkl')
+                # logging.info('Saving optimizer state to %s' % weight_file)
+
+                # weight_values = optimizer.get_weights()
+                # with open(optimizer_file, 'wb') as f:
+                #     pickle.dump(weight_values, f)
+                ckpt_path = manager.save()
+                logging.info('Saved model checkpoint to %s' % ckpt_path)
             if args.save_tflite:
-                tflite_file = join(join(args.save_dir, args.exp_name+'_tflite'), str(i)+'_model.tflite')
+                tflite_file = join(args.save_dir, args.exp_name+'_tflite', str(i)+'_model.tflite')
                 converter = tf.lite.TFLiteConverter.from_keras_model(model)
                 tflite_model = converter.convert()
                 open('tflite_file', 'wb').write(tflite_model)
