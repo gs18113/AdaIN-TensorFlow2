566d99b1728b78453557fd4e7ec7f8c40242410b
gs18113 <seungmin960@gmail.com>
2019-10-31 00:14:12 +0900

    autotune

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
 
-train_data = get_training_set(args.style_dir).repeat().shuffle(30).batch(args.batch_size)
+train_data = get_training_set(args.style_dir).repeat().shuffle(30).batch(args.batch_size).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
 train_iter = iter(train_data)
 
