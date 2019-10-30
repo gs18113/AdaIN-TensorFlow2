1580a824a4ff72100ed0f2e70691e946f35fb3fd
gs18113 <seungmin960@gmail.com>
2019-10-31 00:26:55 +0900

    try recover truncated

--- data.py
+++ data.py
@@ -25,10 +25,7 @@ def get_coco_test_set():
 
 def get_image_from_wikiart(filename):
     image = tf.io.read_file(filename)
-    image = tf.cond(
-        tf.image.is_jpeg(image),
-        lambda: tf.image.decode_jpeg(image, channels=3),
-        lambda: tf.image.decode_png(image, channels=3))
+    image = tf.image.decode_jpeg(image, channels=3, try_recover_truncated=True)
     image_size = tf.shape(image)[:2]
     min_length = tf.reduce_min(image_size)
     image_size = image_size * 512 // min_length
