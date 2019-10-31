6c84a3676fd43f0d1cc854a6e19c48e8d56b228f
gs18113 <seungmin960@gmail.com>
2019-11-01 02:25:20 +0900

    eps

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 def expand_moments_dim(moment):
     return tf.reshape(moment, [-1, 1, 1, tf.shape(moment)[-1]])
 
-def adain(content_feature, style_feature):
+def adain(content_feature, style_feature, eps=1e-5):
     content_mean, content_std = tf.nn.moments(content_feature, axes=[1, 2])
     style_mean, style_std = tf.nn.moments(style_feature, axes=[1, 2])
 
@@ -11,7 +11,7 @@ def adain(content_feature, style_feature):
     # TFLite does not support broadcasting; it is allowed for add, mul, sub, div
     # content_mean = tf.broadcast_to(content_mean, tf.shape(content_feature))
 
-    content_std = expand_moments_dim(content_std)
+    content_std = expand_moments_dim(content_std) + eps
     # TFLite does not support broadcasting; it is allowed for add, mul, sub, div
     # content_std = tf.broadcast_to(content_std, tf.shape(content_feature))
 
@@ -19,7 +19,7 @@ def adain(content_feature, style_feature):
     # TFLite does not support broadcasting; it is allowed for add, mul, sub, div
     # style_mean = tf.broadcast_to(style_mean, tf.shape(content_feature))
 
-    style_std = expand_moments_dim(style_std)
+    style_std = expand_moments_dim(style_std) + eps
     # TFLite does not support broadcasting; it is allowed for add, mul, sub, div
     # style_std = tf.broadcast_to(style_std, tf.shape(content_feature))
 
