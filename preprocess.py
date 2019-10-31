a221b871e64e047d03e019a6faa32e2c72df2bfb
gs18113 <seungmin960@gmail.com>
2019-10-31 11:12:54 +0900

    asdf

--- preprocess.py
+++ preprocess.py
@@ -32,7 +32,7 @@ count = 0
 with tf.io.TFRecordWriter(record_file) as writer:
     with Pool(8) as pool:
         logging.info('Generated pool')
-        for image in pool.imap(glob.glob(style_path)):
+        for image in pool.imap(iter(glob.glob(style_path))):
             if image != None:
                 writer.write(image)
                 count += 1
