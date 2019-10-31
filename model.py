deba448cef5dafaaa9df104a4ef82128d01501e6
gs18113 <seungmin960@gmail.com>
2019-11-01 01:51:56 +0900

    asdf

--- model.py
+++ model.py
@@ -46,8 +46,7 @@ class Net(keras.Model):
 
     def calc_content_loss(self, input, target):
         tf.assert_equal(tf.shape(input), tf.shape(target))
-        tf.print(input)
-        tf.print(target)
+        tf.print(self.mse(input, target))
         return self.mse(input, target)
 
     # Calculate style loss of input & target(after going through VGG-19 layers)
