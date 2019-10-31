7269e29c5e53a39e2d1abf96f16c2a19482764d3
gs18113 <seungmin960@gmail.com>
2019-10-31 11:13:56 +0900

    asdf

--- preprocess.py
+++ preprocess.py
@@ -13,7 +13,7 @@ parser.add_argument('-style_dir', type=str, default='style_images')
 parser.add_argument('-output_dir', type=str, default='processed_images')
 args = parser.parse_args()
 
-def asdf(filename):
+def get_img(filename):
     try:
         image = tf.io.read_file(filename)
         image = tf.image.decode_jpeg(image, channels=3)
@@ -32,7 +32,7 @@ count = 0
 with tf.io.TFRecordWriter(record_file) as writer:
     with Pool(8) as pool:
         logging.info('Generated pool')
-        for image in pool.imap(iter(glob.glob(style_path))):
+        for image in pool.imap(get_img, tqdm(asdf, glob.glob(style_path))):
             if image != None:
                 writer.write(image)
                 count += 1
